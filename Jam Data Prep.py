53c36c5fce5ee2482eb0e86f60aeeaf93771d90b
jakegavel <90931834+jakegavel@users.noreply.github.com>
2022-04-13 11:51:52 -0400

    Add files via upload

--- Jam Data Prep.py
+++ Jam Data Prep.py
@@ -1,11 +1,11 @@
 import pandas as pd
 data = pd.read_csv('jams.tsv', sep = '\t', on_bad_lines='skip', header = 0)
 df = pd.DataFrame(data)
-df = df.drop_duplicates(subset = ['user_id'])
-randuser = df['user_id'].sample(n = 2000, random_state=1)
-df1 = df[df['user_id'].isin(randuser)]
-df1 = df1[['user_id', 'artist', 'title']]
-df1['song'] = df1["title"] + ', by ' + df1['artist']
-df1 = df1[['user_id', 'song']]
-df1.to_csv('jam-sample.csv', index = False)
+df1 = df.drop_duplicates(subset = ['user_id'])
+randuser = df1['user_id'].sample(n = 2000, random_state=1)
+df2 = df[df['user_id'].isin(randuser)]
+df2 = df2[['user_id', 'artist', 'title']]
+df2['song'] = df2["title"] + ', by ' + df2['artist']
+df2 = df2[['user_id', 'song']]
+df2.to_csv('jam-sample.csv', index = False)
 
